18073b7c86c9269d48dbb3f227836a47adcdacbf
Jayce <ashishalex084@gmail.com>
2024-04-29 14:52:34 +0530

    APP

--- app.js
+++ app.js
@@ -18,18 +18,16 @@ const app=express()
 
 app.use(express.json())
 app.use(cors())
-app.use('/api/users',userRouter)
-app.use('/api/employees',employeeRouter)
-app.use('/api/posts',postRouter)
+app.use('/users',userRouter)
+app.use('/employees',employeeRouter)
+app.use('/posts',postRouter)
 
 const __dirname = path.resolve();
 // Serve static files from the 'public' folder
-app.use(express.static(path.join(__dirname,'/dist')))
-app.get('/*', function(req, res) 
-{ res.sendFile(path.join(__dirname ,'/dist/index.html')); });
 
 
-// app.use(express.static(path.join(__dirname, 'uploads')));
+
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 const port = process.env.PORT || 3000
 app.listen(port,()=>{
